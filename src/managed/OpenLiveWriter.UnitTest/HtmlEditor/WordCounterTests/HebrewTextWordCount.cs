52dee5db804f85f5e87fdf664271d9de559353c9
Paul Hatcher <phatcher@munichre.com>
2021-10-16 13:03:30 +0100

    Set explicit OutputType on all assemblies in prep for upgrade tools Update all tests to NUnit and remove references to prior test framework as not required/obsolete

--- src/managed/OpenLiveWriter.UnitTest/HtmlEditor/WordCounterTests/HebrewTextWordCount.cs
+++ src/managed/OpenLiveWriter.UnitTest/HtmlEditor/WordCounterTests/HebrewTextWordCount.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenLiveWriter.HtmlEditor;
+﻿using OpenLiveWriter.HtmlEditor;
 using System;
+using NUnit.Framework;
 
 namespace OpenLiveWriter.UnitTest.HtmlEditor.WordCounterTests
 {
-    [TestClass]
+    [TestFixture]
     public class HebrewTextWordCount
     {
         private static void CountText(string text, int expectedCount, int? expectedCharCount = null)
@@ -14,95 +14,86 @@ namespace OpenLiveWriter.UnitTest.HtmlEditor.WordCounterTests
 
             if (expectedCharCount.HasValue)
             {
-                Assert.AreEqual(expectedCharCount, wordCounter.Chars);
+                Assert.That(wordCounter.Chars, Is.EqualTo(wordCounter.Chars));
             }
         }
 
-        [TestMethod]
+        [Test]
         public void EmptyText_ReturnZero()
         {
             CountText(string.Empty, 0, 0);
         }
 
-        [TestMethod]
+        [Test]
         public void SanityEnglishText()
         {
             CountText("Simple English Text", 3, 19);
         }
 
-        [TestMethod]
+        [Test]
         public void SanityEnglishTextEndsWithPunctuation()
         {
             CountText("Simple English Text.", 3, 20);
         }
 
-
-        [TestMethod]
+        [Test]
         public void SanityEnglishMultiline()
         {
             CountText("This is a " + Environment.NewLine + "multiline text", 5);
         }
 
-        [TestMethod]
+        [Test]
         public void EnglishSeparatedBy()
         {
             CountText("This is a " + Environment.NewLine + "multiline text", 5);
         }
 
-
-
-        [TestMethod]
+        [Test]
         public void CyrillicText()
         {
             CountText("ДЖem", 1, 4);
         }
         
-        [TestMethod]
+        [Test]
         public void OneHebrewWordText()
         {
             CountText("עברית", 1, 5);
         }
 
-        [TestMethod]
+        [Test]
         public void SimpleHebrewText()
         {
             CountText("משפט עם חמש מילים בעברית", 5);
         }
 
-        [TestMethod]
+        [Test]
         public void HebrewMultiline()
         {
             CountText("משפט עם חמש " + "\n" + "מילים בעברית", 5);
         }
 
-
-        [TestMethod]
+        [Test]
         public void MixedHebrewEnglishWords()
         {
-            CountText("מילה בעברית and an english word", 6);            
-
+            CountText("מילה בעברית and an english word", 6);
         }
 
-        [TestMethod]
+        [Test]
         public void MixedHebrewEnglishChars()
         {
             CountText("עבריתenglish", 1, 12);
         }
 
-        [TestMethod]
+        [Test]
         public void OneArabicWordText()
         {
             CountText("عربي", 1, 4);
         }
 
-        [TestMethod]
+        [Test]
         public void SimpleArabicText()
         {
             CountText("اللغة العربية هي أكثر اللغات تحدثاً", 6);
         }
-
-
-
-
     }
 }
